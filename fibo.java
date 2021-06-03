b0a1dfd05aa4539ee52a12830935645e5ff88e99
Pranav Bakre <psbakre@yahoo.com>
2021-06-03 12:07:53 +0530

    fibo

--- fibo.java
+++ fibo.java
@@ -1,5 +1,14 @@
 class fibo {
     public static void main(String []args) {
-        System.out.println("hehe-----");
+        int a=-1;
+        int b=1;
+        int i=0;
+        while(i<10) {
+            int c = a+b;
+            a=b;
+            b=c;
+            System.out.println(c);
+            i++;
+        }
     }
 }
